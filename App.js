49396d1d397012e83b92bd5a90df402ac02aecdc
seokzin <seokzin95@gmail.com>
2021-04-28 00:26:39 +0900

    Implement todo input to push data

--- App.js
+++ App.js
@@ -3,6 +3,34 @@ import TodoList from "./components/TodoList.js";
 import TodoCount from "./components/TodoCount.js";
 import TodoFilter from "./components/TodoFilter.js";
 
-function App() {}
+function App() {
+  this.data = [];
+  this.app = this; // 없애볼까..
+
+  // 컴포넌트 생성
+  const todoInput = new TodoInput(this.app);
+  const todoList = new TodoList(this.data, this.app);
+  const todoCount = new TodoCount(this.app, this.data);
+  const todoFilter = new TodoFilter(this.app, this.data);
+
+  this.saveTodo = (newTodo) => {
+    this.data.push({
+      text: newTodo,
+      id: Date.now(), // 숫자가 의미하는 바는?
+      completed: false,
+    });
+    this.setState(this.data);
+    console.log(this.data);
+  };
+
+  this.setState = (updatedData) => {
+    this.data = updatedData;
+    this.render();
+  };
+
+  this.render = () => {
+    // todoList.setState(this.data);
+  };
+}
 
 export default App;
--- components/TodoInput.js
+++ components/TodoInput.js
@@ -1,3 +1,14 @@
-function TodoInput() {}
+function TodoInput(app) {
+  const $todoInput = document.querySelector(".new-todo");
+
+  this.addTodo = (e) => {
+    const todoText = e.target.value;
+    app.saveTodo(todoText);
+    e.target.value = "";
+  };
+  // this의 원리와 EL에서 this는 원래 뭐였지
+  // change가 값이 있을 때, 엔터 누르면 동작하는 이벤트인데 완전 적합하다고 볼 수 있을까?
+  $todoInput.addEventListener("change", this.addTodo);
+}
 
 export default TodoInput;
