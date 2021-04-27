24cd576b8ee35e1eecb28f5ed76528ef3ac14216
seokzin <seokzin95@gmail.com>
2021-04-27 23:48:57 +0900

    Initial structure

---
+++ App.js
@@ -0,0 +1,8 @@
+import TodoInput from "./components/TodoInput.js";
+import TodoList from "./components/TodoList.js";
+import TodoCount from "./components/TodoCount.js";
+import TodoFilter from "./components/TodoFilter.js";
+
+function App() {}
+
+export default App;
---
+++ components/TodoInput.js
@@ -0,0 +1,3 @@
+function TodoInput() {}
+
+export default TodoInput;
